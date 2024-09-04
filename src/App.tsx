b991969392cf25455f59486d6f5f75475a7dab16
Kirk Flores <113055345+KirSpaceB@users.noreply.github.com>
2024-09-04 11:03:17 -0700

    feat: created the globe

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import {Canvas} from '@react-three/fiber'
+import {Canvas, useLoader} from '@react-three/fiber';
+import * as THREE from 'three';
+import earth_texture from './public/textures/earth_texture.jpg';
 import "./App.css"
 function App() {
-
+  //load texture in local folder
+  const earthTexture = useLoader(THREE.TextureLoader, earth_texture)
   return (
+    
     <>
       <div id="canvas-container">
         <Canvas
           camera={{
-            position:[0,0,8],
+            position:[0,0,10],
             fov:60
           }}
         >
           <mesh>
             <sphereGeometry args={[2,32,32]}/>
-            <meshStandardMaterial color="lightblue"/>
+            <meshStandardMaterial map={earthTexture}/>
           </mesh>
-          <ambientLight intensity={0.5}/>
+          <ambientLight intensity={7}/>
+          <directionalLight position={[5,5,5]} intensity={6}/>
         </Canvas>
       </div>
     </>
