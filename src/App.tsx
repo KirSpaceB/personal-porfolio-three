945e2eb11c28a0b1bc891851bbbf392b56363539
Kirk Flores <113055345+KirSpaceB@users.noreply.github.com>
2024-09-04 10:12:47 -0700

    feat: sphere rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-
-import './App.css'
-
+import {Canvas} from '@react-three/fiber'
+import "./App.css"
 function App() {
 
   return (
     <>
-      <div id='Test'>Hello World</div>
+      <div id="canvas-container">
+        <Canvas
+          camera={{
+            position:[0,0,8],
+            fov:60
+          }}
+        >
+          <mesh>
+            <sphereGeometry args={[2,32,32]}/>
+            <meshStandardMaterial color="lightblue"/>
+          </mesh>
+          <ambientLight intensity={0.5}/>
+        </Canvas>
+      </div>
     </>
   )
 }
