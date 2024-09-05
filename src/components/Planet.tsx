57974f46c8dd2b04fc5cdd78dc9ea1a841fc8cce
Kirk Flores <113055345+KirSpaceB@users.noreply.github.com>
2024-09-05 14:01:11 -0700

    feat: rendering a 2D pane of html to represent a planet:

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -1,9 +1,12 @@
+import { Html } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import '../css/PlanetModal.css';
 import * as THREE from 'three';
+
 export default function Planet({distance, size, speed, color} : {distance:number, size:number, speed:number, color:string}) {
   const planetRef = useRef<THREE.Mesh>(null);
-
+  const [showModal, setShowModal] = useState<boolean>(false);
   useFrame(({ clock }) => {
     if(planetRef.current) {
       const elapsedTime = clock.getElapsedTime();
@@ -12,10 +15,29 @@ export default function Planet({distance, size, speed, color} : {distance:number
     }
   });
 
+  const handleClick = () => {
+    setShowModal(true); // Toggle the modal on click
+  };
+
+  const handleCloseBox = () => {
+    setShowModal(false); // Hide modal when close button is clicked
+  };
+
+
   return (
-    <mesh ref={planetRef}>
+    <mesh ref={planetRef} onClick={handleClick}>
       <sphereGeometry args={[size, 32, 32]} /> {/* Orbiting sphere */}
       <meshStandardMaterial color={color} />
+      {/* Modal logic */}
+      {/* Traditional modal */}
+      {showModal && (
+        <Html position={[2, 1, 0]}>
+        <div id="HelloWorldDiv">
+          <p>test</p>
+          <button id="HelloWorldDivButton" onClick={handleCloseBox}>Close Window</button>
+        </div>
+      </Html>
+      )}
     </mesh>
   );
 }
