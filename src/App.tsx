6a08ad7ee69190948ef9c958c61a565689a9c130
Kirk Flores <113055345+KirSpaceB@users.noreply.github.com>
2024-09-12 11:07:08 -0700

    texture is now added to a planet

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import {Canvas} from '@react-three/fiber';
 import "./App.css"
 import CentralSphere from './components/CentralSphere';
 import Planet from './components/Planet';
-import earthTexture from './public/textures/earth_texture.jpg'
+import earthTexture from './public/textures/earth_texture.jpg';
+import marsTexture from './public/textures/Mars_Map.webp';
+import jupiterTexture from './public/textures/jupitar_texture.jpg';
+import mercuryTexture from './public/textures/mercury_texture.jpg'
 import StarField from './components/StarField';
 
 function App() {
@@ -30,8 +33,10 @@ function App() {
           <group>
             <CentralSphere/>
             <Planet distance={6} size={2} color="white" speed={1} differentAngle={0} texture={earthTexture}/>  {/* Planet 1 */}
-            <Planet distance={8} size={2} color="red" speed={1} differentAngle={Math.PI} />  {/* Planet 2, shifted by 60 degrees */}
-            <Planet distance={10} size={2} color="green" speed={1} differentAngle={Math.PI / 2} /> {/* Planet 3, shifted by 30 degrees */}
+            <Planet distance={8} size={2} color="white" speed={1} differentAngle={Math.PI} texture={marsTexture} />  {/* Planet 2, shifted by 60 degrees */}
+            <Planet distance={10} size={2} color="white" speed={1} differentAngle={Math.PI / 2} texture={jupiterTexture}/> {/* Planet 3, shifted by 30 degrees */}
+            <Planet distance={4} size={2} color="white" speed={1} differentAngle={-Math.PI / 2} texture={mercuryTexture}/> {/* Planet 3, shifted by 30 degrees */}
+
           </group>
         </Canvas>
       </div>
