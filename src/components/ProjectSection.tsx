f205bceed31890b2c23b8b8fab06274963d0ef39
Kirk Flores <113055345+KirSpaceB@users.noreply.github.com>
2024-09-21 18:59:57 -0700

    fix:project section CSS

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -2,18 +2,24 @@ import '../css/ProjectSection.css'
 import GISMap from '../public/GISMap.webp'
 export default function ProjectSection() {
   return (
-    <>
-      <h1>Project Section</h1>
-      <div className="projectItem">
-        <h2>Public Health Surveillance System</h2>
-        <a href="https://github.com/KirSpaceB/public-health-surveillence-system" target="_blank" rel="noopener noreferrer">
-          <img 
-            src={GISMap} 
-            alt="Project Thumbnail" 
-            className="projectImage" 
-          />
-        </a>
+    <div className='projectSectionContainer'>
+      <h1>Projects Section</h1>
+      <div className='projectSectionProjects'>
+        <div className='projectItem'>
+          <h2>Public Health Surveillance System</h2>
+          <a href="https://github.com/KirSpaceB/public-health-surveillence-system" target="_blank" rel="noopener noreferrer">
+            <img src={GISMap} alt="Project Thumbnail" className="projectImage" />
+          </a>
+        </div>
+
+        <div className='projectItem'>
+          <h2>Project 2</h2>
+          <a href="https://github.com/KirSpaceB/public-health-surveillence-system" target="_blank" rel="noopener noreferrer">
+            <img src={GISMap} alt="Project Thumbnail" className="projectImage" />
+          </a>
+        </div>
       </div>
-    </>
+    </div>
+
   )
 }
